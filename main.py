24c1976b6bc29c637060073ef61b3672e0c8bd2d
Vincent <v.wawak1@gmail.com>
2023-11-07 09:35:50 -0500

    feat: Added player and player movement
    TODO: Made speed fixed in one direction on key press. Restrict player to predefined squares

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ clock = pygame.time.Clock()
 #Set a boolean variable for whether the app is running or not
 running = True
 
+snake_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+snake_head = pygame.Rect(snake_pos.x - 10, snake_pos.y - 10, 20, 20)
+speed = 10
 
 #Start the loop for the game
 while running:
@@ -28,19 +31,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        else:
-            print(event)
 
     # Add bg color
     screen.fill(colors.BACKGROUND_COLOR)
+    # Add play space
 
     # Render here...
+
+    pygame.draw.rect(screen, colors.GREEN, snake_head)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        snake_pos.y -= speed
+    if keys[pygame.K_s]:
+        snake_pos.y += speed
+    if keys[pygame.K_a]:
+        snake_pos.x -= speed
+    if keys[pygame.K_d]:
+        snake_pos.x += speed
+    snake_head.update(snake_pos.x, snake_pos.y, 20, 20)
+
 
     # .flip() to display work on screen
     pygame.display.flip()
 
     # Determines framerate
-    clock.tick(60)
+    clock.tick(20)
 
 
 
